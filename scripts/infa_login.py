840324c1f9f2a3cdaa9f9e01ee681a4cc33f4971
Ratnakar Patel <99405844+patelratnakar@users.noreply.github.com>
2023-12-06 22:52:47 +0530

    Update infa_login.py

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 
 #Define the Login Call Function
@@ -17,5 +18,7 @@ def loginCall(userName, passWord):
     response = requests.request("POST", url, headers=headers, data = payload_json)
     response_json = response.json() if response and response.status_code == 200 else None
     infa_session_id = response_json['userInfo']['sessionId']
+    name = "sessionId"
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as file:
+        print(f'{name}={infa_session_id}', file=file)
     return infa_session_id
-    
