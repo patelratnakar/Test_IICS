d95ad35e2e11f39ad5029e15a18cb9cb62c3fa02
Ratnakar Patel <99405844+patelratnakar@users.noreply.github.com>
2023-12-06 15:15:37 +0530

    Update infa_login.py

--- scripts/infa_login.py
+++ scripts/infa_login.py
@@ -1,25 +1,20 @@
 import requests
-import os
 import json
 
-URL = os.environ['IICS_LOGIN_URL']
-USERNAME = os.environ['IICS_USERNAME']
-PASSWORD = os.environ['IICS_PASSWORD']
 
-URL = "https://dm2-us.informaticacloud.com"
-BODY = {"username": USERNAME, "password": PASSWORD}
-
-r = requests.post(url = URL, json = BODY)
-
-if r.status_code != 200:
-   # print("Caught exception: " + r.text)
-infa_session_id = response_json['userInfo']['sessionId']
-# extracting data in JSON format
-#data = r.json()
-
-# Set session tokens to the environment
-env_file = os.getenv('GITHUB_ENV')
-
-with open(env_file, "a") as myfile:
-    myfile.write("sessionId=" + data['userInfo']['sessionId'] + "\n")
-    
+#Define the Login Call Function
+def loginCall(userName, passWord):
+    url = "https://dm2-us.informaticacloud.com"
+    payload = {
+        "username" : "me111089@global.org.in",
+        "password" : "Anil1980**"
+    }
+    payload_json = json.dumps(payload)
+    headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            }
+    response = requests.request("POST", url, headers=headers, data = payload_json)
+    response_json = response.json() if response and response.status_code == 200 else None
+    infa_session_id = response_json['userInfo']['sessionId']
+    return infa_session_id
